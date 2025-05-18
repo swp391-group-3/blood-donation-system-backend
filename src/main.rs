44199b8672767c1c5c4a4205c0afab3b9760de09
akagiyuu <khanghuynh0245@gmail.com>
2025-05-18 19:59:14 +0700

    feat: init tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{
+    EnvFilter, fmt::time::ChronoLocal, layer::SubscriberExt, util::SubscriberInitExt,
+};
+
 fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .pretty()
+                .with_timer(ChronoLocal::rfc_3339()),
+        )
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::DEBUG.into())
+                .from_env_lossy(),
+        )
+        .init();
+
     println!("Hello, world!");
 }
