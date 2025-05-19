50a1216f260d875a75ab7f9895f81ea1ffc972a7
akagiyuu <khanghuynh0245@gmail.com>
2025-05-19 15:11:53 +0700

    feat(config): add cors domain

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -6,10 +6,17 @@ const fn default_port() -> u16 {
     3000
 }
 
+fn default_cors_domain() -> String {
+    "http://localhost:3000".to_string()
+}
+
 #[derive(Deserialize)]
 pub struct Config {
     #[serde(default = "default_port")]
     pub port: u16,
+
+    #[serde(default = "default_cors_domain")]
+    pub cors_domain: String,
 }
 
 pub static CONFIG: LazyLock<Config> = LazyLock::new(|| {
