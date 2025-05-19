2f8a575d0c3e81ad988f3f4305cecd944bae3b68
akagiyuu <khanghuynh0245@gmail.com>
2025-05-19 11:27:45 +0700

    feat(api/controller): add controllers path definition

--- src/api/controller/mod.rs
+++ src/api/controller/mod.rs
@@ -1,3 +1,12 @@
 mod ping;
 
+use std::sync::Arc;
+
+use axum::{Router, routing};
 pub use ping::*;
+
+use super::state::ApiState;
+
+pub async fn build(router: Router<Arc<ApiState>>) -> Router<Arc<ApiState>> {
+    router.route("/", routing::get(ping))
+}
