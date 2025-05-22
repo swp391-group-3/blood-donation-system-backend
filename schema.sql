32ba055bda3c40fde1d1ff841e243c376331fc22
akagiyuu <khanghuynh0245@gmail.com>
2025-05-22 16:29:26 +0700

    feat(schema): add blood storage table

--- schema.sql
+++ schema.sql
@@ -11,6 +11,14 @@ CREATE TABLE IF NOT EXISTS blood_components(
     name varchar(32) UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS blood_storages(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name varchar(32) UNIQUE NOT NULL,
+    blood_id uuid REFERENCES bloods(id),
+    component_id uuid REFERENCES blood_components(id),
+    amount int NOT NULL DEFAULT 0
+);
+
 CREATE TABLE IF NOT EXISTS accounts(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     role int NOT NULL,
