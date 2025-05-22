d8839ca812307f991a9f9a78a512e873bc0081e8
akagiyuu <khanghuynh0245@gmail.com>
2025-05-22 16:07:01 +0700

    feat(schema): add tags table

--- schema.sql
+++ schema.sql
@@ -33,3 +33,15 @@ CREATE TABLE IF NOT EXISTS comments(
     content text NOT NULL,
     created_at timestamp NOT NULL DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS tags(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name varchar(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS blog_tags(
+    blog_id uuid NOT NULL REFERENCES blogs(id),
+    tag_id uuid NOT NULL REFERENCES tags(id),
+
+    PRIMARY KEY (blog_id, tag_id)
+);
