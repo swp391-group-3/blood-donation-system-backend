838cc26351b60ff3da0b16fba272c5bb66e71c9b
akagiyuu <khanghuynh0245@gmail.com>
2025-05-22 16:03:52 +0700

    feat(schema): add comments table

--- schema.sql
+++ schema.sql
@@ -25,3 +25,11 @@ CREATE TABLE IF NOT EXISTS blogs(
     content text NOT NULL,
     created_at timestamp NOT NULL DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS comments(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    blog_id uuid NOT NULL REFERENCES blogs(id),
+    account_id uuid NOT NULL REFERENCES accounts(id),
+    content text NOT NULL,
+    created_at timestamp NOT NULL DEFAULT now()
+);
