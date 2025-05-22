961343674818f90873d0fe833b88c9c78a36e767
akagiyuu <khanghuynh0245@gmail.com>
2025-05-22 11:34:38 +0700

    feat(schema): add accounts table

--- schema.sql
+++ schema.sql
@@ -5,3 +5,16 @@ CREATE TABLE IF NOT EXISTS bloods(
     name varchar(16) UNIQUE NOT NULL,
     expired_time int NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS accounts(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    role int NOT NULL,
+    email varchar(128) UNIQUE NOT NULL,
+    phone varchar(16) UNIQUE NOT NULL,
+    gender int NOT NULL,
+    address text NOT NULL,
+    birthday date NOT NULL,
+    blood_id uuid REFERENCES bloods(id),
+    is_active boolean NOT NULL DEFAULT true,
+    update_at timestamp NOT NULL DEFAULT now()
+);
