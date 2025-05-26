28535f355afdd58b541d5c7da15321ce11b15de7
akagiyuu <khanghuynh0245@gmail.com>
2025-05-26 11:43:41 +0700

    fix(schema): remove blood expired time

--- schema.sql
+++ schema.sql
@@ -2,8 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS bloods(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name varchar(16) UNIQUE NOT NULL,
-    expired_time int NOT NULL
+    name varchar(16) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS blood_components(
