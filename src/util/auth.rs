83f8c1a3544e38168120b90b6ebf03ebeaeb4052
akagiyuu <khanghuynh0245@gmail.com>
2025-05-27 01:34:19 +0700

    feat: add claim extractor

--- src/util/auth.rs
+++ src/util/auth.rs
@@ -1,12 +1,17 @@
-use std::sync::LazyLock;
+use std::sync::{Arc, LazyLock};
 
+use axum::{RequestPartsExt, extract::FromRequestParts, http::request::Parts};
+use axum_extra::{
+    TypedHeader,
+    headers::{Authorization, authorization::Bearer},
+};
 use chrono::Local;
-use jsonwebtoken::{DecodingKey, EncodingKey, Header, encode};
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation, encode};
 use rand::distr::{Alphanumeric, SampleString};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::config::CONFIG;
+use crate::{config::CONFIG, error::AuthError, state::ApiState};
 
 pub static ENCODING_KEY: LazyLock<EncodingKey> =
     LazyLock::new(|| EncodingKey::from_secret(CONFIG.jwt.secret.as_bytes()));
@@ -19,7 +24,7 @@ pub fn random_password() -> String {
     Alphanumeric.sample_string(&mut rand::rng(), BCRYPT_LENGTH)
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Claims {
     pub sub: Uuid,
     pub exp: u64,
@@ -36,4 +41,26 @@ pub fn generate_token(id: Uuid) -> jsonwebtoken::errors::Result<String> {
         },
         &ENCODING_KEY,
     )
+}
+
+impl FromRequestParts<Arc<ApiState>> for Claims {
+    type Rejection = crate::error::Error;
+
+    async fn from_request_parts(
+        parts: &mut Parts,
+        _: &Arc<ApiState>,
+    ) -> Result<Self, Self::Rejection> {
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await?;
+
+        let token = bearer.token();
+        let token = jsonwebtoken::decode::<Claims>(token, &DECODING_KEY, &Validation::default())
+            .map_err(|error| {
+                tracing::error!(error = ?error);
+                AuthError::InvalidAuthToken
+            })?;
+
+        Ok(token.claims)
+    }
 }
