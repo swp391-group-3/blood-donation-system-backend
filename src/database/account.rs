5c51e6e6a6317d9c10211b1d2b530e83a4e7cf0e
akagiyuu <khanghuynh0245@gmail.com>
2025-05-26 21:09:06 +0700

    feat(database/account): add get password function

--- src/database/account.rs
+++ src/database/account.rs
@@ -18,3 +18,12 @@ pub async fn create(
     .fetch_one(executor)
     .await
 }
+
+pub async fn get_password(email: &str, executor: impl PgExecutor<'_>) -> Result<String> {
+    sqlx::query_scalar!(
+        "SELECT password FROM accounts WHERE email = $1 LIMIT 1",
+        email
+    )
+    .fetch_one(executor)
+    .await
+}
