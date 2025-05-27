40a97dfbbdd53bae51d9d187c6061460da8cb9ca
akagiyuu <khanghuynh0245@gmail.com>
2025-05-27 09:38:34 +0700

    feat(state): add http client
    Signed-off-by: akagiyuu <khanghuynh0245@gmail.com>

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use openidconnect::reqwest;
 use sqlx::PgPool;
 
 use crate::config::CONFIG;
@@ -7,12 +8,21 @@ use crate::config::CONFIG;
 #[allow(unused)]
 pub struct ApiState {
     pub database_pool: PgPool,
+    pub http_client: reqwest::Client,
 }
 
 impl ApiState {
     pub async fn new() -> Arc<Self> {
         let database_pool = PgPool::connect(&CONFIG.server.database_url).await.unwrap();
 
-        Arc::new(Self { database_pool })
+        let http_client = reqwest::ClientBuilder::new()
+            .redirect(reqwest::redirect::Policy::none())
+            .build()
+            .unwrap();
+
+        Arc::new(Self {
+            database_pool,
+            http_client,
+        })
     }
 }
