d42bbfe31db421c8ad1b79d6dc0799add0734ed6
akagiyuu <khanghuynh0245@gmail.com>
2025-05-27 09:54:48 +0700

    feat(config/google): add redirect url

--- src/config/google.rs
+++ src/config/google.rs
@@ -4,6 +4,10 @@ fn default_issuer_url() -> String {
     "https://accounts.google.com".to_string()
 }
 
+fn default_redirect_url() -> String {
+    "http://localhost:3000/auth/google/authorized".to_string()
+}
+
 #[derive(Deserialize)]
 pub struct GoogleConfig {
     pub client_id: String,
@@ -12,6 +16,9 @@ pub struct GoogleConfig {
 
     #[serde(default = "default_issuer_url")]
     pub issuer_url: String,
+
+    #[serde(default = "default_redirect_url")]
+    pub redirect_url: String,
 }
 
 impl GoogleConfig {
