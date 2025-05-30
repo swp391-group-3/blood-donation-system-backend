392a5f74197b5a29e5028a1fe25b273a42d02d0d
akagiyuu <khanghuynh0245@gmail.com>
2025-05-30 15:01:19 +0700

    feat(middleware): use macro_rules for authorization

--- src/middleware/authorize.rs
+++ src/middleware/authorize.rs
@@ -1,36 +1,29 @@
-use std::sync::Arc;
+#[macro_export]
+macro_rules! authorize {
+    ($($role:expr),+) => {
+        async |
+            claims: $crate::util::auth::Claims,
+            State(state): axum::extract::State<std::sync::Arc<$crate::state::ApiState>>,
+            request: axum::extract::Request,
+            next: axum::middleware::Next,
+        | -> $crate::error::Result<axum::response::Response> {
+            let auth_status = $crate::database::account::get_auth_status(claims.sub, &state.database_pool)
+                .await?
+                .ok_or($crate::error::AuthError::InvalidAuthToken)?;
 
-use axum::{
-    extract::{Request, State},
-    middleware::Next,
-    response::Response,
-};
+            if !auth_status.is_active {
+                return Err($crate::error::AuthError::ActivationRequired.into());
+            }
 
-use crate::{
-    database::{self, account::Role},
-    error::{AuthError, Result},
-    state::ApiState,
-    util::auth::Claims,
-};
+            $(
+                if $role == auth_status.role {
+                    return Ok(next.run(request).await)
+                }
+            )*
 
-pub async fn authorize(
-    roles: &[Role],
-    claims: Claims,
-    State(state): State<Arc<ApiState>>,
-    request: Request,
-    next: Next,
-) -> Result<Response> {
-    let auth_status = database::account::get_auth_status(claims.sub, &state.database_pool)
-        .await?
-        .ok_or(AuthError::InvalidAuthToken)?;
-
-    if !auth_status.is_active {
-        return Err(AuthError::ActivationRequired.into());
-    }
-
-    if !roles.contains(&auth_status.role) {
-        return Err(AuthError::MissingPermission.into());
-    }
-
-    Ok(next.run(request).await)
+            Err($crate::error::AuthError::MissingPermission.into())
+        }
+    };
 }
+
+pub use authorize;
