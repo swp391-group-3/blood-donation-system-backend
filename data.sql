d4930cbf66bafe51e324ec9b368928641a33d778
akagiyuu <khanghuynh0245@gmail.com>
2025-06-03 13:50:25 +0700

    feat: use schema and data file instead

---
+++ data.sql
@@ -0,0 +1,30 @@
+INSERT INTO roles(name) VALUES
+    ('member'),
+    ('staff'),
+    ('admin');
+
+INSERT INTO donation_types(name) VALUES
+    ('whole_blood'),
+    ('power_red'),
+    ('platelet'),
+    ('plasma');
+
+INSERT INTO blood_groups(name) VALUES
+    ('O+'),
+    ('O-'),
+    ('A+'),
+    ('A-'),
+    ('B+'),
+    ('B-'),
+    ('AB+'),
+    ('AB-');
+
+INSERT INTO blood_components(name) VALUES
+    ('red_cell'),
+    ('platelet'),
+    ('plasma');
+
+INSERT INTO request_priorities(name) VALUES
+    ('low'),
+    ('medium'),
+    ('high');
---
+++ schema.sql
@@ -0,0 +1,133 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS blood_groups(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name varchar(4) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS roles(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name varchar(16) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS accounts(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    role_id uuid NOT NULL REFERENCES roles(id),
+    email varchar(128) UNIQUE NOT NULL,
+    password varchar(72) NOT NULL,
+    phone varchar(16) UNIQUE,
+    name varchar(64),
+    gender int,
+    address text,
+    birthday date,
+    blood_group_id uuid REFERENCES blood_groups(id),
+    is_active boolean NOT NULL DEFAULT false,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS blogs(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    account_id uuid NOT NULL REFERENCES accounts(id),
+    title text NOT NULL,
+    content text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS comments(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    blog_id uuid NOT NULL REFERENCES blogs(id),
+    account_id uuid NOT NULL REFERENCES accounts(id),
+    content text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS tags(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name varchar(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS blog_tags(
+    blog_id uuid NOT NULL REFERENCES blogs(id),
+    tag_id uuid NOT NULL REFERENCES tags(id),
+
+    PRIMARY KEY (blog_id, tag_id)
+);
+
+CREATE TABLE IF NOT EXISTS request_priorities(
+    id serial PRIMARY KEY,
+    name varchar(16) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS blood_requests(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    staff_id uuid NOT NULL REFERENCES accounts(id),
+    priority_id int NOT NULL REFERENCES request_priorities(id),
+    blood_group_id uuid NOT NULL REFERENCES blood_groups(id),
+    title text NOT NULL,
+    max_people int NOT NULL,
+    start_time timestamptz NOT NULL,
+    end_time timestamptz NOT NULL,
+    is_active bool NOT NULL DEFAULT true,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS appointments(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    request_id uuid NOT NULL REFERENCES blood_requests(id),
+    member_id uuid NOT NULL REFERENCES accounts(id),
+
+    UNIQUE (request_id, member_id)
+);
+
+CREATE TABLE IF NOT EXISTS questions(
+    id serial PRIMARY KEY,
+    content text NOT NULL,
+    is_active bool NOT NULL DEFAULT true
+);
+
+CREATE TABLE IF NOT EXISTS answers(
+    question_id int NOT NULL REFERENCES questions(id),
+    appointment_id uuid NOT NULL REFERENCES appointments(id),
+    content text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS healths(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    appointment_id uuid NOT NULL REFERENCES appointments(id),
+    temperature real NOT NULL,
+    weight real NOT NULL,
+    upper_blood_pressure int NOT NULL,
+    lower_blood_pressure int NOT NULL,
+    heart_pulse int NOT NULL,
+    hemoglobin real NOT NULL,
+    is_good_health bool NOT NULL,
+    note text,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS donation_types(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name varchar(16) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS donations(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    appointment_id uuid NOT NULL REFERENCES appointments(id),
+    type_id uuid NOT NULL REFERENCES donation_types(id),
+    amount int NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS blood_components(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name varchar(16) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS blood_bags(
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    donation_id uuid NOT NULL REFERENCES donations(id),
+    component_id uuid NOT NULL REFERENCES blood_components(id),
+    is_used bool NOT NULL DEFAULT false,
+    amount int NOT NULL,
+    expired_time timestamptz NOT NULL
+);
