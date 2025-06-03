2a4f54b2ecdd2100cab4a2d5be5d47fe1442c5de
akagiyuu <khanghuynh0245@gmail.com>
2025-06-03 13:52:50 +0700

    feat: add clorinde auto build

---
+++ build.rs
@@ -0,0 +1,18 @@
+use clorinde::{Error, config::Config};
+
+#[allow(clippy::result_large_err)]
+fn main() -> Result<(), Error> {
+    let queries_path = "queries";
+    let schema_file = "schema.sql";
+
+    let cfg = Config::builder()
+        .name("database")
+        .destination("database")
+        .build();
+
+    println!("cargo:rerun-if-changed={queries_path}");
+    println!("cargo:rerun-if-changed={schema_file}");
+    clorinde::gen_managed(&[schema_file], cfg)?;
+
+    Ok(())
+}
