4d3f9349fbfa2df52a60c4302d2bf8783bf5a9d9
akagiyuu <khanghuynh0245@gmail.com>
2025-06-03 13:33:04 +0700

    feat: update database queries

--- src/database/health.rs
+++ src/database/health.rs
@@ -5,27 +5,58 @@ use uuid::Uuid;
 
 use sqlx::{PgExecutor, Result};
 
+#[derive(Deserialize, ToSchema)]
+pub struct CreateHealth {
+    pub temperature: f32,
+    pub weight: f32,
+    pub upper_blood_pressure: i32,
+    pub lower_blood_pressure: i32,
+    pub heart_pulse: i32,
+    pub hemoglobin: f32,
+    pub is_good_health: bool,
+    pub note: Option<String>,
+}
+
 pub async fn create(
     appointment_id: Uuid,
-    temperature: f32,
-    weight: f32,
-    height: f32,
-    is_good_health: bool,
-    note: Option<&str>,
+    params: &CreateHealth,
     executor: impl PgExecutor<'_>,
 ) -> Result<Uuid> {
     sqlx::query_scalar!(
         r#"
-            INSERT INTO healths(appointment_id, temperature, weight, height, is_good_health, note)
-            VALUES($1, $2, $3, $4, $5, $6)
+            INSERT INTO healths(
+                appointment_id,
+                temperature,
+                weight,
+                upper_blood_pressure,
+                lower_blood_pressure,
+                heart_pulse,
+                hemoglobin,
+                is_good_health,
+                note
+            )
+            VALUES(
+                $1,
+                $2,
+                $3,
+                $4,
+                $5,
+                $6,
+                $7,
+                $8,
+                $9
+            )
             RETURNING id
         "#,
         appointment_id,
-        temperature,
-        weight,
-        height,
-        is_good_health,
-        note
+        params.temperature,
+        params.weight,
+        params.upper_blood_pressure,
+        params.lower_blood_pressure,
+        params.heart_pulse,
+        params.hemoglobin,
+        params.is_good_health,
+        params.note
     )
     .fetch_one(executor)
     .await
@@ -34,9 +65,13 @@ pub async fn create(
 #[derive(Serialize, ToSchema)]
 pub struct Health {
     pub id: Uuid,
+    pub appointment_id: Uuid,
     pub temperature: f32,
     pub weight: f32,
-    pub height: f32,
+    pub upper_blood_pressure: i32,
+    pub lower_blood_pressure: i32,
+    pub heart_pulse: i32,
+    pub hemoglobin: f32,
     pub is_good_health: bool,
     pub note: Option<String>,
     pub created_at: DateTime<Utc>,
@@ -49,7 +84,7 @@ pub async fn get_by_appoinment_id(
     sqlx::query_as!(
         Health,
         r#"
-            SELECT id, temperature, weight, height, is_good_health, note, created_at
+            SELECT *
             FROM healths
             WHERE appointment_id = $1
         "#,
@@ -66,7 +101,7 @@ pub async fn get_by_member_id(
     sqlx::query_as!(
         Health,
         r#"
-            SELECT id, temperature, weight, height, is_good_health, note, created_at
+            SELECT *
             FROM healths
             WHERE appointment_id IN (SELECT id FROM appointments WHERE member_id = $1)
         "#,
@@ -80,7 +115,10 @@ pub async fn get_by_member_id(
 pub struct UpdateParams {
     pub temperature: Option<f32>,
     pub weight: Option<f32>,
-    pub height: Option<f32>,
+    pub upper_blood_pressure: Option<i32>,
+    pub lower_blood_pressure: Option<i32>,
+    pub heart_pulse: Option<i32>,
+    pub hemoglobin: Option<f32>,
     pub is_good_health: Option<bool>,
     pub note: Option<String>,
 }
@@ -92,15 +130,13 @@ pub async fn update(id: Uuid, params: &UpdateParams, executor: impl PgExecutor<'
             SET
                 temperature = COALESCE($2, temperature),
                 weight = COALESCE($3, weight),
-                height = COALESCE($4, height),
-                is_good_health = COALESCE($5, is_good_health),
-                note = COALESCE($6, note)
+                is_good_health = COALESCE($4, is_good_health),
+                note = COALESCE($5, note)
             WHERE id = $1
         "#,
         id,
         params.temperature,
         params.weight,
-        params.height,
         params.is_good_health,
         params.note
     )
