3b6c2f20769a1f4e1b169a1336e18fca7515c135
Lunarcd <nhatanhth0212@gmail.com>
2025-06-06 14:17:36 +0700

    feat(blood-request): add create validation

--- backend/src/controller/blood_request/create.rs
+++ backend/src/controller/blood_request/create.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use axum::{Json, extract::State};
+use axum_valid::Valid;
 use chrono::{DateTime, Utc};
 use ctypes::{BloodGroup, RequestPriority};
 use database::{
@@ -13,7 +14,7 @@ use utoipa::ToSchema;
 use uuid::Uuid;
 use validator::Validate;
 
-use crate::{error::Result, state::ApiState, util::jwt::Claims};
+use crate::{error::Result, state::ApiState, util::custom_validator, util::jwt::Claims};
 
 #[derive(Deserialize, ToSchema, Mapper, Validate)]
 #[schema(as = blood_request::create::Request)]
@@ -25,10 +26,13 @@ use crate::{error::Result, state::ApiState, util::jwt::Claims};
 pub struct Request {
     pub blood_group: BloodGroup,
     pub priority: RequestPriority,
+    #[validate(length(min = 1))]
     pub title: String,
     #[validate(range(min = 1))]
     pub max_people: u16,
+    #[validate(custom(function = "custom_validator::date_time_must_after_now"))]
     pub start_time: DateTime<Utc>,
+    #[validate(custom(function = "custom_validator::date_time_must_after_now"))]
     pub end_time: DateTime<Utc>,
 }
 
@@ -46,7 +50,7 @@ pub struct Request {
 pub async fn create(
     state: State<Arc<ApiState>>,
     claims: Claims,
-    Json(request): Json<Request>,
+    Valid(Json(request)): Valid<Json<Request>>,
 ) -> Result<Json<Uuid>> {
     let database = state.database_pool.get().await?;
 
--- backend/src/util/custom_validator.rs
+++ backend/src/util/custom_validator.rs
@@ -1,10 +1,10 @@
 use chrono::{DateTime, Utc};
 use validator::ValidationError;
 
-pub fn date_time_must_after_now(value: DateTime<Utc>) -> Result<(), ValidationError> {
+pub fn date_time_must_after_now(value: &DateTime<Utc>) -> Result<(), ValidationError> {
     let now = Utc::now();
 
-    if value <= now {
+    if value <= &now {
         return Err(ValidationError::new("date_time"));
     }
 
