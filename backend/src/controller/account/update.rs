de72533f6a871ad157908145916ead61e3bddee1
ChiBao <baotochi87@gmail.com>
2025-06-07 08:55:38 +0700

    feat(account): api for update

---
+++ backend/src/controller/account/update.rs
@@ -0,0 +1,51 @@
+use std::sync::Arc;
+
+use axum::{Json, extract::State};
+use chrono::NaiveDate;
+use ctypes::Gender;
+use serde::Deserialize;
+use utoipa::ToSchema;
+use uuid::Uuid;
+
+use crate::{error::Result, state::ApiState, util::jwt::Claims};
+use database::{
+    client::Params,
+    queries::{self, account::UpdateParams},
+};
+
+#[derive(Deserialize, ToSchema)]
+pub struct Request {
+    pub phone: Option<String>,
+    pub name: Option<String>,
+    pub gender: Option<Gender>,
+    pub address: Option<String>,
+    pub birthday: Option<NaiveDate>,
+}
+
+impl Request {
+    pub fn with_account_id(
+        self,
+        account_id: Uuid,
+    ) -> queries::account::UpdateParams<String, String, String> {
+        UpdateParams {
+            id: account_id,
+            phone: self.phone,
+            name: self.name,
+            gender: self.gender,
+            address: self.address,
+            birthday: self.birthday,
+        }
+    }
+}
+
+pub async fn update(
+    state: State<Arc<ApiState>>,
+    claims: Claims,
+    Json(request): Json<Request>,
+) -> Result<()> {
+    let database = state.database_pool.get().await?;
+    queries::account::update()
+        .params(&database, &request.with_account_id(claims.sub))
+        .await?;
+    Ok(())
+}
