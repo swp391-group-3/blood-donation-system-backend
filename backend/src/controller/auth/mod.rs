630b616180b5dada61b9951ed33f7792e289c83a
Lunarcd <nhatanhth0212@gmail.com>
2025-06-10 21:02:12 +0700

    feat(auth): group oauth2

--- backend/src/controller/auth/mod.rs
+++ backend/src/controller/auth/mod.rs
@@ -2,6 +2,7 @@ mod activate;
 pub mod google;
 mod login;
 pub mod microsoft;
+pub mod oauth2;
 mod register;
 mod status;
 
@@ -20,13 +21,9 @@ pub fn build() -> Router<Arc<ApiState>> {
     Router::new()
         .route("/auth/register", routing::post(register))
         .route("/auth/login", routing::post(login))
-        .route("/auth/google", routing::get(google::google))
-        .route("/auth/google/authorized", routing::get(google::authorized))
-        .route("/auth/microsoft", routing::get(microsoft::microsoft))
+        .route("/auth/oauth2/{provider}", routing::get(oauth2::oauth2))
         .route(
-            "/auth/microsoft/authorized",
-            routing::get(microsoft::authorized),
+            "/auth/oauth2/{provider}/authorized",
+            routing::get(oauth2::authorized),
         )
-        .route("/auth/activate", routing::post(activate))
-        .route("/auth/status", routing::get(status))
 }
