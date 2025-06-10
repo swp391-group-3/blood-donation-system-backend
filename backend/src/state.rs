9e523745c0928d1ae7154c27e38e96727e24e558
Lunarcd <nhatanhth0212@gmail.com>
2025-06-10 20:41:20 +0700

    feat(state): init new oidc_clients map

--- backend/src/state.rs
+++ backend/src/state.rs
@@ -1,43 +1,50 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use database::{deadpool_postgres, tokio_postgres::NoTls};
 
 use crate::{
-    config::{oidc::OpenIdConnectConfig, server::ServerConfig},
+    config::{
+        CONFIG,
+        oidc::{OpenIdConnectConfig, Provider},
+        server::ServerConfig,
+    },
     util::{bcrypt::BcryptService, jwt::JwtService, oidc::OpenIdConnectClient},
 };
 
 #[allow(unused)]
 pub struct ApiState {
     pub database_pool: deadpool_postgres::Pool,
-    pub google_client: OpenIdConnectClient,
-    pub microsoft_client: OpenIdConnectClient,
+    pub oidc_clients: HashMap<Provider, OpenIdConnectClient>,
     pub jwt_service: JwtService,
-    pub bcrypt_service: BcryptService,
 }
 
 impl ApiState {
-    pub async fn new(config: &ServerConfig) -> Arc<Self> {
+    pub async fn new() -> Arc<Self> {
         let mut database_config = deadpool_postgres::Config::new();
-        database_config.url = Some(config.database_url.clone());
+        database_config.url = Some(CONFIG.database_url.clone());
         let database_pool = database_config
             .create_pool(Some(deadpool_postgres::Runtime::Tokio1), NoTls)
             .unwrap();
 
-        let google_client = OpenIdConnectClient::from_config(OpenIdConnectConfig::new("google"))
-            .await
-            .unwrap();
-        let microsoft_client =
-            OpenIdConnectClient::from_config(OpenIdConnectConfig::new("microsoft"))
-                .await
-                .unwrap();
+        let oidc_clients = CONFIG
+            .open_id_connect
+            .clients
+            .iter()
+            .map(|(provider, config)| {
+                (
+                    provider,
+                    OpenIdConnectClient::from_config(
+                        config,
+                        CONFIG.open_id_connect.oauth2_redirect_url,
+                    ),
+                )
+            })
+            .collect();
 
         Arc::new(Self {
             database_pool,
-            google_client,
-            microsoft_client,
+            oidc_clients,
             jwt_service: Default::default(),
-            bcrypt_service: Default::default(),
         })
     }
 }
