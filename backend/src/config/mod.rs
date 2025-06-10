ebb008de03734f7ce1014e02d67d66d26b922843
Lunarcd <nhatanhth0212@gmail.com>
2025-06-10 20:24:42 +0700

    feat(config): define global config

--- backend/src/config/mod.rs
+++ backend/src/config/mod.rs
@@ -1,4 +1,29 @@
 pub mod bcrypt;
 pub mod jwt;
 pub mod oidc;
-pub mod server;
+
+use std::sync::LazyLock;
+
+use config::Environment;
+use serde::Deserialize;
+
+use crate::config::{bcrypt::BcryptConfig, jwt::JwtConfig, oidc::OpenIdConnectConfig};
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub database_url: String,
+    pub port: u16,
+    pub bcrypt: BcryptConfig,
+    pub jwt: JwtConfig,
+    pub open_id_connect: OpenIdConnectConfig,
+}
+
+pub static CONFIG: LazyLock<Config> = LazyLock::new(|| {
+    ::config::Config::builder()
+        .add_source(::config::File::with_name("config.dev").required(false))
+        .add_source(::config::File::with_name("config.production").required(false))
+        .build()
+        .unwrap()
+        .try_deserialize()
+        .unwrap()
+});
