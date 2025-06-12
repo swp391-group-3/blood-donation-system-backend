c6c80fe97fa92d6052af03c2735107afbda3f7f2
BaoTo12 <baotochi87@gmail.com>
2025-06-12 10:38:18 +0700

    feat(oauth2): save immediate step in session

--- backend/src/controller/auth/oauth2.rs
+++ backend/src/controller/auth/oauth2.rs
@@ -71,28 +71,25 @@ pub async fn authorized(
 
     let email = claims.email().expect("Account must have email").as_str();
 
-    queries::account::oauth2_register()
+    if let Some(account) = queries::account::get_by_email()
         .bind(&database, &email)
-        .await
-        .map_err(|error| {
+        .opt()
+        .await?
+    {
+        let id = account.id;
+
+        let cookie = state.jwt_service.new_credential(id).map_err(|error| {
             tracing::error!(error =? error);
-            AuthError::AccountExisted
+            AuthError::InvalidAuthToken
         })?;
 
-    let id = queries::account::get_id_and_password()
-        .bind(&database, &email)
-        .one()
-        .await
-        .expect("Account must be created in previous step to get here")
-        .id;
+        return Ok((jar.add(cookie), Redirect::to(&CONFIG.frontend_url)));
+    }
 
-    let cookie = state.jwt_service.new_credential(id).map_err(|error| {
-        tracing::error!(error =? error);
-        AuthError::InvalidAuthToken
-    })?;
+    session.insert(KEY, email);
 
     Ok((
-        jar.add(cookie),
-        Redirect::to(&CONFIG.open_id_connect.frontend_redirect_url),
+        jar,
+        Redirect::to(&format!("{}/auth/complete", CONFIG.frontend_url)),
     ))
 }
