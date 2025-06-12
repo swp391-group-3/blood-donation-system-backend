3ba43c3ab2763bad5a15e6fd74fdbd2351054156
Lunarcd <nhatanhth0212@gmail.com>
2025-06-12 14:56:05 +0700

    feat: add register link to login page

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -20,12 +20,10 @@ import * as api from '@/lib/api';
 import { toast } from 'sonner';
 import { useMutation } from '@tanstack/react-query';
 import { Loader2 } from 'lucide-react';
-import { FcGoogle } from 'react-icons/fc';
-import Image from 'next/image';
 import { OAuth2 } from '../components/oauth2';
 import { Separator } from '@/components/separator';
 
-export default function LoginPage() {
+const LoginForm = () => {
     const { mutate: login, status } = useMutation({
         mutationFn: api.auth.login,
         onError: (error) => toast.error(error.message),
@@ -37,79 +35,82 @@ export default function LoginPage() {
     });
 
     return (
+        <Card>
+            <CardHeader>
+                <CardTitle className="text-2xl text-center">Login</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit((values) => login(values))}
+                    >
+                        <div className="flex flex-col gap-6">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem className="grid gap-2">
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} required />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem className="grid gap-2">
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                type="password"
+                                                {...field}
+                                                required
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+
+                            {status === 'pending' ? (
+                                <Button disabled>
+                                    <Loader2 className="animate-spin" />
+                                    Loading
+                                </Button>
+                            ) : (
+                                <Button type="submit" className="w-full py-5">
+                                    Login
+                                </Button>
+                            )}
+                        </div>
+                    </form>
+                </Form>
+                <Separator text="or" />
+                <OAuth2 />
+            </CardContent>
+        </Card>
+    );
+};
+
+export default function LoginPage() {
+    return (
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm">
                 <div className="flex flex-col gap-6">
-                    <Card>
-                        <CardHeader>
-                            <CardTitle className="text-2xl text-center">
-                                Login
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <Form {...form}>
-                                <form
-                                    onSubmit={form.handleSubmit((values) =>
-                                        login(values),
-                                    )}
-                                >
-                                    <div className="flex flex-col gap-6">
-                                        <FormField
-                                            control={form.control}
-                                            name="email"
-                                            render={({ field }) => (
-                                                <FormItem className="grid gap-2">
-                                                    <FormLabel>Email</FormLabel>
-                                                    <FormControl>
-                                                        <Input
-                                                            {...field}
-                                                            required
-                                                        />
-                                                    </FormControl>
-                                                    <FormMessage />
-                                                </FormItem>
-                                            )}
-                                        />
-                                        <FormField
-                                            control={form.control}
-                                            name="password"
-                                            render={({ field }) => (
-                                                <FormItem className="grid gap-2">
-                                                    <FormLabel>
-                                                        Password
-                                                    </FormLabel>
-                                                    <FormControl>
-                                                        <Input
-                                                            type="password"
-                                                            {...field}
-                                                            required
-                                                        />
-                                                    </FormControl>
-                                                    <FormMessage />
-                                                </FormItem>
-                                            )}
-                                        />
-
-                                        {status === 'pending' ? (
-                                            <Button disabled>
-                                                <Loader2 className="animate-spin" />
-                                                Loading
-                                            </Button>
-                                        ) : (
-                                            <Button
-                                                type="submit"
-                                                className="w-full py-5"
-                                            >
-                                                Login
-                                            </Button>
-                                        )}
-                                    </div>
-                                </form>
-                            </Form>
-                            <Separator text="or" />
-                            <OAuth2 />
-                        </CardContent>
-                    </Card>
+                    <LoginForm />
+                    <div className="mt-4 text-center text-sm">
+                        Don't have an account?{' '}
+                        <Link
+                            href="/auth/register"
+                            className="underline underline-offset-4"
+                        >
+                            Register
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
