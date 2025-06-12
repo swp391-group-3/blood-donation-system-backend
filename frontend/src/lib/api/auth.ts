aee6a0ec59c3d62c24ce1a14ef9dc66d9bd8a8a3
Lunarcd <nhatanhth0212@gmail.com>
2025-06-12 13:12:32 +0700

    feat: add register wrapper

--- frontend/src/lib/api/auth.ts
+++ frontend/src/lib/api/auth.ts
@@ -1,8 +1,87 @@
-'use client';
-
 import z from 'zod';
 import { API_URL, fetchWrapper, throwIfError } from '.';
 import { redirect } from 'next/navigation';
+
+export const blood_groups = [
+    'o_plus',
+    'o_minus',
+    'a_plus',
+    'a_minus',
+    'b_plus',
+    'b_minus',
+    'a_b_plus',
+    'a_b_minus',
+] as const;
+export const blood_groups_with_label = [
+    {
+        label: 'O+',
+        value: 'o_plus',
+    },
+
+    {
+        label: 'O-',
+        value: 'o_minus',
+    },
+    {
+        label: 'A+',
+        value: 'a_plus',
+    },
+    {
+        label: 'A-',
+        value: 'a_minus',
+    },
+    {
+        label: 'B+',
+        value: 'b_plus',
+    },
+    {
+        label: 'B-',
+        value: 'b_minus',
+    },
+    {
+        label: 'AB+',
+        value: 'a_b_plus',
+    },
+    {
+        label: 'AB-',
+        value: 'a_b_minus',
+    },
+];
+
+export const genders = ['male', 'female'] as const;
+export const gender_with_labels = genders.map((gender) => ({
+    label: gender,
+    value: gender,
+}));
+
+export const registerSchema = z.object({
+    email: z.string().email(),
+    password: z.string().nonempty({
+        message: 'Password must not be empty.',
+    }),
+    phone: z
+        .string()
+        .regex(/0[\d]{9,9}/, { message: 'Phone must consist of 10 number' }),
+    address: z.string().min(1, { message: 'Address must be provided.' }),
+    birthday: z.date().refine((date) => date.getTime() < Date.now(), {
+        message: 'Date must be in the past',
+    }),
+    blood_group: z.enum(blood_groups),
+    gender: z.enum(genders),
+});
+
+export const register = async (values: z.infer<typeof registerSchema>) => {
+    const response = await fetchWrapper('auth/register', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+    });
+
+    await throwIfError(response);
+};
 
 export const loginSchema = z.object({
     email: z
@@ -13,7 +92,7 @@ export const loginSchema = z.object({
     }),
 });
 
-export async function login(values: z.infer<typeof loginSchema>) {
+export const login = async (values: z.infer<typeof loginSchema>) => {
     const response = await fetchWrapper('auth/login', {
         method: 'POST',
         headers: {
@@ -24,10 +103,10 @@ export async function login(values: z.infer<typeof loginSchema>) {
     });
 
     await throwIfError(response);
-}
+};
 
 export type Provider = 'google' | 'microsoft';
 
-export async function oauth2(provider: Provider) {
+export const oauth2 = (provider: Provider) => {
     redirect(`${API_URL}/oauth2/${provider}`);
-}
+};
--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,19 +1,19 @@
 export const API_URL = process.env.API_URL || 'http://localhost:3000';
 
-export async function throwIfError(response: Response) {
+export const throwIfError = async (response: Response) => {
     if (!response.ok) {
         const error = await response.text();
 
         throw new Error(error);
     }
-}
+};
 
-export async function deserialize<T>(response: Response): Promise<T> {
+export const deserialize = async <T>(response: Response): Promise<T> => {
     await throwIfError(response);
 
     const data: T = await response.json();
     return data;
-}
+};
 
 export const fetchWrapper = (url: RequestInfo | URL, init?: RequestInit) => {
     const apiUrl = `${API_URL}${url}`;
