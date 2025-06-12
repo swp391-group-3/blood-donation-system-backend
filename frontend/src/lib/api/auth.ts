3b1c6a82dfe990dbeb707f5a73f04691bce677f6
Lunarcd <nhatanhth0212@gmail.com>
2025-06-12 15:32:50 +0700

    feat: add oauth2Complete api call

--- frontend/src/lib/api/auth.ts
+++ frontend/src/lib/api/auth.ts
@@ -92,3 +92,23 @@ export type Provider = 'google' | 'microsoft';
 export const oauth2 = (provider: Provider) => {
     redirect(`${API_URL}/oauth2/${provider}`);
 };
+
+export const oauth2CompleteSchema = registerSchema.omit({
+    email: true,
+    password: true,
+});
+
+export const oauth2Complete = async (
+    values: z.infer<typeof oauth2CompleteSchema>,
+) => {
+    const response = await fetchWrapper('/oauth2/complete', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+    });
+
+    await throwIfError(response);
+};
