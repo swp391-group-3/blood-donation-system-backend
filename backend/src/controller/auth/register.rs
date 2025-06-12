0ce1ead01fb8b3692ebd3b7fdbccb9dc5ce90c6f
BaoTo12 <baotochi87@gmail.com>
2025-06-12 10:42:02 +0700

    feat(api/auth): full register

--- backend/src/controller/auth/register.rs
+++ backend/src/controller/auth/register.rs
@@ -3,10 +3,13 @@ use std::sync::Arc;
 use axum::{Json, extract::State};
 use axum_extra::extract::CookieJar;
 use axum_valid::Valid;
+use chrono::NaiveDate;
+use ctypes::{BloodGroup, Gender};
 use database::{
     client::Params,
     queries::{self, account::RegisterParams},
 };
+use model_mapper::Mapper;
 use serde::Deserialize;
 use utoipa::ToSchema;
 use validator::Validate;
@@ -17,13 +20,20 @@ use crate::{
     state::ApiState,
 };
 
-#[derive(Deserialize, ToSchema, Validate)]
+#[derive(Deserialize, ToSchema, Validate, Mapper)]
 #[schema(as = auth::register::request)]
+#[mapper(into, ty = RegisterParams::<String, String, String, String, String>)]
 pub struct Request {
     #[validate(email)]
     pub email: String,
     #[validate(length(min = 1))]
     pub password: String,
+    pub phone: String,
+    pub name: String,
+    pub gender: Gender,
+    pub address: String,
+    pub birthday: NaiveDate,
+    pub blood_group: BloodGroup,
 }
 
 #[utoipa::path(
@@ -35,7 +45,7 @@ pub struct Request {
 pub async fn register(
     state: State<Arc<ApiState>>,
     jar: CookieJar,
-    Valid(Json(request)): Valid<Json<Request>>,
+    Valid(Json(mut request)): Valid<Json<Request>>,
 ) -> Result<CookieJar> {
     let database = state.database_pool.get().await?;
 
@@ -46,15 +56,10 @@ pub async fn register(
                 AuthError::InvalidLoginData
             })?
             .to_string();
+    request.password = password;
 
     let id = queries::account::register()
-        .params(
-            &database,
-            &RegisterParams {
-                email: &request.email,
-                password,
-            },
-        )
+        .params(&database, &request.into())
         .one()
         .await
         .map_err(|error| {
