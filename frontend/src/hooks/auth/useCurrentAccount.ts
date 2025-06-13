9cbdd6bae745a0c136bea5cd92e53497eac06935
Lunarcd <nhatanhth0212@gmail.com>
2025-06-13 08:27:44 +0700

    fix(hook/auth): return awaited deserialize

--- frontend/src/hooks/auth/useCurrentAccount.ts
+++ frontend/src/hooks/auth/useCurrentAccount.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { deserialize, fetchWrapper } from '@/lib/api';
 import { Account } from '@/lib/api/dto/account';
 import { useQuery } from '@tanstack/react-query';
@@ -7,7 +9,7 @@ export const useCurrentAccount = () => {
         queryFn: async () => {
             const response = await fetchWrapper('/auth/me');
 
-            return deserialize<Account>(response);
+            return await deserialize<Account>(response);
         },
         queryKey: ['auth', 'me'],
     });
