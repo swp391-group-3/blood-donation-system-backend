885a855f21059046f4a4eeff339d9cf7c057fd78
Lunarcd <nhatanhth0212@gmail.com>
2025-06-14 21:47:53 +0700

    feat: responsive account indicator

--- frontend/src/components/account-picture.tsx
+++ frontend/src/components/account-picture.tsx
@@ -6,8 +6,8 @@ export interface AccountPictureProps {
 
 export const AccountPicture = ({ name }: AccountPictureProps) => {
     return (
-        <Avatar className="rounded-lg">
-            <AvatarFallback className="bg-rose-400/20 text-rose-500 rounded font-medium">
+        <Avatar className="rounded-lg size-full">
+            <AvatarFallback className="bg-rose-400/20 text-rose-500 text-[1em] rounded font-medium">
                 {name[0]}
             </AvatarFallback>
         </Avatar>
