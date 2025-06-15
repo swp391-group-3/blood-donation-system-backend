8dcf2bfde1e7510d19bb3c10b7b55d1246a3c51c
namdang-fdp <namdangcoder@gmail.com>
2025-06-15 21:03:57 +0700

    feat: change color func to utils

--- frontend/src/app/staff/blood-storage/page.tsx
+++ frontend/src/app/staff/blood-storage/page.tsx
@@ -70,8 +70,6 @@ import {
     CalendarIcon,
     ShieldAlert,
     Droplet,
-    TrendingUp,
-    TrendingDown,
     Eye,
     Edit,
     RefreshCw,
@@ -85,6 +83,7 @@ import {
 } from '../../../../constants/sample-data';
 import { useForm } from 'react-hook-form';
 import { TabsContent } from '@radix-ui/react-tabs';
+import { getDemandColor , getPriorityColor, getStatusColor } from "../../../utils/colorUtils"
 
 export const requestBloodSchema = z.object({
     bloodType: z.string().min(1, 'Please select the blood type'),
@@ -124,51 +123,6 @@ export default function BloodBagsPage() {
     const [dateRange, setDateRange] = useState<DateRange | undefined>(
         undefined,
     );
-
-    const getStatusColor = (status: string) => {
-        switch (status) {
-            case 'Available':
-                return 'bg-emerald-100 text-emerald-800 border-emerald-200';
-            case 'Reserved':
-                return 'bg-blue-100 text-blue-800 border-blue-200';
-            case 'Used':
-                return 'bg-gray-100 text-gray-800 border-gray-200';
-            case 'Expiring Soon':
-                return 'bg-amber-100 text-amber-800 border-amber-200';
-            case 'Expired':
-                return 'bg-red-100 text-red-800 border-red-200';
-            default:
-                return 'bg-gray-100 text-gray-800 border-gray-200';
-        }
-    };
-
-    const getPriorityColor = (priority: string) => {
-        switch (priority) {
-            case 'urgent':
-                return 'bg-red-100 text-red-800 border-red-200';
-            case 'high':
-                return 'bg-orange-100 text-orange-800 border-orange-200';
-            case 'normal':
-                return 'bg-green-100 text-green-800 border-green-200';
-            default:
-                return 'bg-gray-100 text-gray-800 border-gray-200';
-        }
-    };
-
-    const getDemandColor = (demand: string) => {
-        switch (demand) {
-            case 'very-high':
-                return 'text-red-600';
-            case 'high':
-                return 'text-orange-600';
-            case 'medium':
-                return 'text-yellow-600';
-            case 'low':
-                return 'text-green-600';
-            default:
-                return 'text-gray-600';
-        }
-    };
 
     const handleSubmitRequest = (e: React.FormEvent) => {
         // come in future
