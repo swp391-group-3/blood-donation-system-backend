72737170b570cf5942ccab95e5d9fe97486457f3
ChiBao <baotochi87@gmail.com>
2025-06-15 08:33:32 +0700

    style(admin): style sidebar

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,16 +1,21 @@
+'use client';
+import { Button } from '@/components/ui/button'
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { cn } from '@/lib/utils'
+import { Calendar, Heart, Home, Inbox, Search, Settings } from "lucide-react"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 const items = [
     {
         title: "Home",
-        url: "#",
+        url: "/admin",
         icon: Home,
     },
     {
         title: "Staff",
-        url: "#",
+        url: "/admin/staff",
         icon: Inbox,
     },
     {
@@ -31,21 +36,42 @@ const items = [
 ]
 
 function AppSidebar() {
+    const pathName = usePathname();
+
+    console.log(pathName);
+
+
     return (
-        <Sidebar>
-            <SidebarHeader>
-                Admin Dashboard
+        <Sidebar className='flex bg-gray-50'>
+            <SidebarHeader className="sticky top-0 h-dvd bg-white border-zinc-300 flex flex-col">
+                <div className='flex items-center space-x-2 mt-3 mb-3'>
+                    <Heart className="h-6 w-6 text-red-600" />
+                    <span className='text-xl font-bold text-zinc-950'>
+                        Admin Dashboard
+                    </span>
+                </div>
             </SidebarHeader>
             <SidebarContent>
-                <SidebarGroupContent>
+                <SidebarGroupContent className='px-3.5'>
                     <SidebarMenu>
-                        {items.map(item => (
+                        {items.map((item, index) => (
                             <SidebarMenuItem key={item.title}>
                                 <SidebarMenuButton asChild>
-                                    <a href={item.url}>
-                                        <item.icon />
-                                        <span>{item.title}</span>
-                                    </a>
+                                    <Button
+                                        asChild
+                                        key={index}
+                                        variant="ghost"
+                                        className={cn(
+                                            'w-full justify-start text-zinc-600 hover:text-zinc-900 hover:bg-gray-200 py-4.5 my-0.5 transition',
+                                            pathName === item.url &&
+                                            'bg-red-50 text-red-600 hover:bg-red-100 hover:shadow',
+                                        )}
+                                    >
+                                        <Link href={item.url}>
+                                            <item.icon />
+                                            {item.title}
+                                        </Link>
+                                    </Button>
                                 </SidebarMenuButton>
                             </SidebarMenuItem>
                         ))}
@@ -64,7 +90,6 @@ function layout({ children }: Readonly<{ children: React.ReactNode }>) {
         <SidebarProvider>
             <AppSidebar />
             <main>
-                <SidebarTrigger />
                 {children}
             </main>
         </SidebarProvider>
---
+++ frontend/src/app/admin/staff/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div>Staff page</div>
+  )
+}
+
+export default Page
